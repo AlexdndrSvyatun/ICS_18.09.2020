f20fd769725bc6d8b3cff1c8a3d596c3c8b827fd
Святун Олександр <alecksandrtd@gmail.com>
2020-10-29 20:19:12 +0200

    commit

---
+++ Laba 5/random.py
@@ -0,0 +1,16 @@
+import random
+
+names = ["Inna", "Roma", "Sasha", "Katya"]
+verbs = ["do", "fail", "lost", "finish"]
+nounce = ["work","test","homework"]
+
+random.shuffle(names)
+n = names[1]
+
+random.shuffle(verbs)
+v = verbs[1]
+
+random.shuffle(food)
+o = nounce [1]
+
+print(n + "" + v + "" + o)
