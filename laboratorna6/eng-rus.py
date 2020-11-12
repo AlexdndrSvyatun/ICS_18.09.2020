23e834060bcdb77a331e6625b3c4e6f48d04c49b
Святун Олександр <alecksandrtd@gmail.com>
2020-11-12 20:07:04 +0200

    commit

---
+++ laboratorna6/eng-rus.py
@@ -0,0 +1,13 @@
+class Vocabulary():
+    def _init_(self, english_value, russian_value):
+        self.english_value = english_value
+        self.russian_value = [russian_value]
+
+words = {
+    "just":["только","просто"],
+    "way":["путь"],
+    "trick":["трюк"],
+    "vanish":["исчезать"]
+}
+        
+print(words)        
